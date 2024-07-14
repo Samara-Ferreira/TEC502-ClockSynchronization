6fb4e0a6e67c4328242792c9fd51f02ac654b995
Samara <sferreira@ecomp.uefs.br>
2024-07-13 23:42:10 -0300

    :sparkles: feat: adição das configurações da rede
    - Adição dos endereços IP, porta e verificação se o servidor está ativo ou não.

---
+++ src/NetworkConfig.py
@@ -0,0 +1,7 @@
+
+host_list = {
+    "1": {"host": "172.16.103.1", "port": 5551, "active": False},
+    "2": {"host": "172.16.103.3", "port": 5552, "active": False},
+    "3": {"host": "172.16.103.3", "port": 5553, "active": False},
+    "4": {"host": "172.16.103.4", "port": 5554, "active": False}
+}
