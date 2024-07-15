ecdea4ecd88fc54afcef27739a3c4f03e16521e9
Samara <sferreira@ecomp.uefs.br>
2024-07-15 01:57:52 -0300

    :sparkles: feat: adição da função de limpar o terminal

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,17 @@
 
 from NetworkConfig import host_list
 from API import app, Id
+from os import system, name
+
+
+def get_clear_prompt() -> None:
+    if name == 'nt':
+        system('cls') or None
+    else:
+        system('clear') or None
 
 
 if __name__ == "__main__":
+    # get_clear_prompt()
+
     app.run(host="0.0.0.0", port=host_list[Id]["port"], debug=False, threaded=True)
