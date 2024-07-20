8b1eec830247e3c9476b462d4e25adc1f8102863
Samara <sferreira@ecomp.uefs.br>
2024-07-20 01:46:38 -0300

    :sparkles: feat: adição do tempo atual dos relógios

--- src/NetworkConfig.py
+++ src/NetworkConfig.py
@@ -1,7 +1,21 @@
 
+# host_list = {
+#     "1": {"host": "172.16.103.1", "port": 5551, "active": False},
+#     "2": {"host": "172.16.103.2", "port": 5552, "active": False},
+#     "3": {"host": "172.16.103.3", "port": 5553, "active": False},
+#     "4": {"host": "172.16.103.4", "port": 5554, "active": False}
+# }
+
 host_list = {
-    "1": {"host": "172.16.103.1", "port": 5551, "active": False},
-    "2": {"host": "172.16.103.3", "port": 5552, "active": False},
-    "3": {"host": "172.16.103.3", "port": 5553, "active": False},
-    "4": {"host": "172.16.103.4", "port": 5554, "active": False}
+    "1": {"host": "192.168.0.111", "port": 5551, "active": False, "time": -1},
+    "2": {"host": "192.168.0.111", "port": 5552, "active": False, "time": -1},
+    "3": {"host": "192.168.0.111", "port": 5553, "active": False, "time": -1},
+    "4": {"host": "192.168.0.111", "port": 5554, "active": False, "time": -1}
 }
+
+# host_list = {
+#     "1": {"host": "10.65.135.114", "port": 5551, "active": False, "time": -1},
+#     "2": {"host": "10.65.135.114", "port": 5552, "active": False, "time": -1},
+#     "3": {"host": "10.65.135.114", "port": 5553, "active": False, "time": -1},
+#     "4": {"host": "10.65.135.114", "port": 5554, "active": False, "time": -1}
+# }
