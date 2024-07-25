dc3a54f6fb510e15f8050f2cca4ad5bf92e4b6a4
Samara <sferreira@ecomp.uefs.br>
2024-07-25 00:50:37 -0300

    :sparkles: feat: adição de novas rotas e dos comentários

--- src/API.py
+++ src/API.py
@@ -1,55 +1,61 @@
+"""Módulo para a API do sistema."""
 
-from flask import Flask, jsonify
-from Node import Node
 from os import getenv
+from Node import Node
 from threading import Thread
+from flask import Flask, jsonify
 
 
-# Variáveis globais
 Id = getenv("ID", "1")
 Node = Node(Id)
 
 app = Flask(__name__)
 
 
-# Rota para verificar se é o líder
-@app.route("/get_leader", methods=["GET"])
-def get_leader():
-    return jsonify(Node.get_info())
-
-
-# Rota para eleger um lider
 @app.route("/elect_leader", methods=["POST"])
 def elect_leader():
+    """Rota para eleger um líder."""
     Thread(target=Node.set_leader).start()
     return jsonify({"message": "Líder eleito com sucesso! ID do líder:" + str(Node.id_node)})
 
 
-# Rota para confirmar o líder
 @app.route("/<string:id>/confirm_leader", methods=["POST"])
 def confirm_leader(id):
+    """Rota para confirmar o líder."""
     return jsonify({"message": Node.confirm_leader(id)})
 
 
-# Rota para obter o horário atual dos relógios
-@app.route("/get_time", methods=["GET"])
-def get_time():
-    return jsonify({"message": Node.control_clock.get_time()})
-
-
-# Rota para receber a média dos relógios
 @app.route("/<int:time_sync>/<int:final_time>/receive_times", methods=["POST"])
 def receive_times(time_sync, final_time):
+    """Rota para receber a média dos tempos dos relógios."""
     return jsonify({"message": Node.receive_times(time_sync, final_time)})
 
 
-# Rota para mudar o tempo do relógio
 @app.route("/<int:time>/change_time", methods=["POST"])
 def change_time(time):
+    """Rota para mudar o tempo do relógio."""
     return jsonify({"message": Node.control_clock.clock.set_time(time)})
 
 
-# Rota para mudar o drift do relógio
 @app.route("/<float:drift>/change_drift", methods=["POST"])
 def change_drift(drift):
+    """Rota para mudar o drift do relógio."""
     return jsonify({"message": Node.control_clock.clock.set_drift(drift)})
+
+
+@app.route("/change_sync", methods=["POST"])
+def change_sync():
+    """Rota para mudar a flag de sincronização."""
+    return jsonify({"message": Node.control_clock.set_is_sync()})
+
+
+@app.route("/get_leader", methods=["GET"])
+def get_leader():
+    """Rota para obter o líder."""
+    return jsonify(Node.get_info())
+
+
+@app.route("/get_time", methods=["GET"])
+def get_time():
+    """Rota para obter o horário atual de um relógio."""
+    return jsonify({"message": Node.control_clock.get_time()})
